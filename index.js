1a0cb8f680b7f34f60f06b93f1faf0201d48d230
sayed8901 <sayed91515@gmail.com>
2023-05-19 00:17:54 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+
+const port = process.env.PORT || 5000;
+const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+
+
+app.get('/', (req, res) => {
+    res.send('FairyWorld is running')
+})
+
+app.listen(port, () => {
+    console.log(`fairyWorld is running: ${port}`);
+})
